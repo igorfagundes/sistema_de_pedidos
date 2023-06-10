62be81c92d9e706c85ec32ebf77fb59be2e3ba3b
igor fagundes pontes <128741930+igorfagundes@users.noreply.github.com>
2023-06-10 18:45:10 -0300

    iniciando sistema de pedidos
    iniciando sistema de pedidos criando as classes model cliente e estoque

---
+++ src/main/java/com/thecode/sistemadepedido/sistemadepedido/SistemadepedidoApplication.java
@@ -0,0 +1,13 @@
+package com.thecode.sistemadepedido.sistemadepedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadepedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadepedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thecode/sistemadepedido/sistemadepedido/SistemadepedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecode.sistemadepedido.sistemadepedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadepedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
